ed21dbf2cf70ac19fd856e6cbb43e006103fa0d3
ab1nash10 <ab1nashkumar9815@gmail.com>
2024-10-01 13:19:27 +0530

    (setup): define app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/appLayout";
+import JobListing from "./Pages/jobListing";
+import JobPage from "./Pages/jobs";
+import LandingPages from "./Pages/landingPages";
+import MyJobs from "./Pages/myJobs";
+import OnBoarding from "./Pages/onBoarding";
+import PostJobs from "./Pages/postJobs";
+import SavedJobs from "./Pages/savedJobs";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPages />,
+      },
+      {
+        path: "/onboard",
+        element: <OnBoarding />,
+      },
+      {
+        path: "/myjobs",
+        element: <MyJobs />,
+      },
+      {
+        path: "/jobs/:id",
+        element: <JobPage />,
+      },
+      {
+        path: "/savedjobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/jobslisting",
+        element: <JobListing />,
+      },
+      {
+        path: "/postjobs",
+        element: <PostJobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <div>
-        <p>Hello</p>
-        <Button>login</Button>
-        <h1 className="text-4xl">hello</h1>
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
