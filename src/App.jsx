a9fa0065acc0d5236b51d52e7f50c7c2ce7e6bd5
ab1nash10 <ab1nashkumar9815@gmail.com>
2024-10-01 11:40:38 +0530

    (setup): integrate vite with shadcnui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hello</p>
+        <Button>login</Button>
+        <h1 className="text-4xl">hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
